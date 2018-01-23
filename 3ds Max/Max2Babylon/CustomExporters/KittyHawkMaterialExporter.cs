fccb1250b90f012b5156b99625ea2cd0475ed617
Selmar Kok <skok@asobostudio.com>
2018-01-23 12:47:29 +0100

    material export update

--- 3ds Max/Max2Babylon/CustomExporters/KittyHawkMaterialExporter.cs
+++ 3ds Max/Max2Babylon/CustomExporters/KittyHawkMaterialExporter.cs
@@ -1,28 +1,29 @@
 using Autodesk.Max;
 using BabylonExport.Entities;
+using GLTFExport.Entities;
 
 namespace Max2Babylon
 {
-    public class KittyHawkMaterialExporter : IMaterialExporter
+    public class KittyHawkMaterialExporter : IGLTFMaterialExporter
     {
         readonly ClassIDWrapper class_ID = new ClassIDWrapper(0x53196aaa, 0x57b6ad6a);
 
         ClassIDWrapper IMaterialExporter.MaterialClassID => class_ID;
 
-        bool IMaterialExporter.IsBabylonExporter => true;
-
-        bool IMaterialExporter.IsGltfExporter => false;
+        //bool IMaterialExporter.IsBabylonExporter => true;
+        //
+        //bool IMaterialExporter.IsGltfExporter => false;
 
         public KittyHawkMaterialExporter() { }
 
         // getValueByNameUsingParamBlock2Internal(*pMtl,_T("DiffuseTex"),TYPE_FILENAME,&Filename,TV);
 
-        BabylonMaterial IMaterialExporter.ExportBabylonMaterial(IIGameMaterial material)
+        GLTFMaterial IGLTFMaterialExporter.ExportGLTFMaterial(IIGameMaterial material)
         {
-            BabylonPBRMetallicRoughnessMaterial babylonMaterial = new BabylonPBRMetallicRoughnessMaterial();
+            GLTFMaterial gltfMaterial = new GLTFMaterial();
 
-            babylonMaterial.name = material.MaterialName;
-            babylonMaterial.id = material.MaxMaterial.GetGuid().ToString();
+            gltfMaterial.name = material.MaterialName;
+            gltfMaterial.id = material.MaxMaterial.GetGuid().ToString();
 
             int numProps = material.IPropertyContainer.NumberOfProperties;
             IIGameProperty[] properties = new IIGameProperty[numProps];
@@ -39,10 +40,10 @@ namespace Max2Babylon
                 IIParamBlock2 paramBlock = material.MaxMaterial.GetParamBlock(i);
                 paramBlocks[i] = paramBlock;
             }
+            
+            ProcessParamBlocks(gltfMaterial, material);
 
-            ProcessParamBlocks(babylonMaterial, material);
-
-            return babylonMaterial;
+            return gltfMaterial;
 
             // to get an overview, for debug purposes
             /*
@@ -64,7 +65,7 @@ namespace Max2Babylon
             */
         }
 
-        void ProcessParamBlocks(BabylonPBRMetallicRoughnessMaterial babylonMaterial, IIGameMaterial material)
+        void ProcessParamBlocks(GLTFMaterial babylonMaterial, IIGameMaterial material)
         {
             int numParamBlocks = material.MaxMaterial.NumParamBlocks;
             for (int i = 0; i < numParamBlocks; ++i)
@@ -91,7 +92,7 @@ namespace Max2Babylon
             }
         }
 
-        void ProcessParamBlock_SHADER(BabylonPBRMetallicRoughnessMaterial material, IIParamBlock2 paramBlock)
+        void ProcessParamBlock_SHADER(GLTFMaterial material, IIParamBlock2 paramBlock)
         {
             int numParams = paramBlock.NumParams;
             for(int i = 0; i < numParams; ++i)
@@ -107,18 +108,19 @@ namespace Max2Babylon
                         string code = paramBlock.GetStr(paramId, 0, 0);
                         if (code.ToUpperInvariant() == "STANDARD")
                         {
-
+                            material.alphaMode = GLTFMaterial.AlphaMode.OPAQUE.ToString();
                         }
                         else if (code.ToUpperInvariant() == "GLASS")
                         {
-                            material.alpha = 0.3f;
+                            material.alphaMode = GLTFMaterial.AlphaMode.BLEND.ToString();
+                            SetBaseColorAlpha(material, 0.3f);
                         }
                         break;
                 }
             }
         }
 
-        void ProcessParamBlock_PARAMS(BabylonPBRMetallicRoughnessMaterial material, IIParamBlock2 paramBlock)
+        void ProcessParamBlock_PARAMS(GLTFMaterial material, IIParamBlock2 paramBlock)
         {
             int numParams = paramBlock.NumParams;
             for (int i = 0; i < numParams; ++i)
@@ -133,7 +135,7 @@ namespace Max2Babylon
                             continue;// todo: throw warning
 
                         IPoint4 baseColor = paramBlock.GetPoint4(paramId, 0, 0);
-                        material.baseColor = new[] { baseColor.X, baseColor.Y, baseColor.Z };
+                        material.SetBaseColorFactor(baseColor.X, baseColor.Y, baseColor.Z);
 
                         break;
                     case "EMISSIVE":
@@ -141,7 +143,7 @@ namespace Max2Babylon
                             continue;// todo: throw warning
 
                         IPoint4 emissive = paramBlock.GetPoint4(paramId, 0, 0);
-                        material.emissive = new[] { emissive.X, emissive.Y, emissive.Z };
+                        material.SetEmissiveFactor(emissive.X, emissive.Y, emissive.Z);
 
                         break;
                     case "ROUGHNESS":
@@ -149,7 +151,7 @@ namespace Max2Babylon
                             continue;// todo: throw warning
 
                         float roughness = paramBlock.GetFloat(paramId, 0, 0);
-                        material.roughness = roughness;
+                        material.SetRoughnessFactor(roughness);
 
                         break;
                     case "METALLIC":
@@ -157,7 +159,7 @@ namespace Max2Babylon
                             continue;// todo: throw warning
 
                         float metallic = paramBlock.GetFloat(paramId, 0, 0);
-                        material.metallic = metallic;
+                        material.SetMetallicFactor(metallic);
 
                         break;
                     case "NORMALSCALE":
@@ -170,11 +172,11 @@ namespace Max2Babylon
                 }
             }
         }
-        void ProcessParamBlock_PARAMS2(BabylonPBRMetallicRoughnessMaterial material, IIParamBlock2 paramBlock)
+        void ProcessParamBlock_PARAMS2(GLTFMaterial material, IIParamBlock2 paramBlock)
         {
 
         }
-        void ProcessParamBlock_TEXTURES(BabylonPBRMetallicRoughnessMaterial material, IIParamBlock2 paramBlock)
+        void ProcessParamBlock_TEXTURES(GLTFMaterial material, IIParamBlock2 paramBlock)
         {
             int numParams = paramBlock.NumParams;
             for (int i = 0; i < numParams; ++i)
@@ -207,10 +209,135 @@ namespace Max2Babylon
                 }
             }
         }
-        void ProcessParamBlock_FLAGS(BabylonPBRMetallicRoughnessMaterial material, IIParamBlock2 paramBlock)
+        void ProcessParamBlock_FLAGS(GLTFMaterial material, IIParamBlock2 paramBlock)
         {
-
+            
         }
+    }
+
+    static class GLTFMaterialExtensions
+    {
+        public static class Defaults
+        {
+            public static readonly float[] BaseColorFactor = new float[] { 1, 1, 1, 1 };
+            public static readonly float[] EmissiveFactor = new float[] { 0, 0, 0 };
+            public const float RoughnessFactor = 1.0f;
+            public const float MetallicFactor = 1.0f;
+        }
+
+        #region GLTFMaterial helper functions
+        // Pretty much all material parameters are optional, which means a lot of variables (nested) are null or have null parents before we set them.
+        // These functions exist to make this a bit easier to read and use the gltf spec defaults when variables have to be initialized.
+        // In addition, we try to minimize what we export by setting variables to null when they're defaults.
+
+        public static void SetBaseColorFactorAlpha(this GLTFMaterial gltfMaterial, float alpha)
+        {
+            if (alpha == Defaults.EmissiveFactor[3])
+            {
+                if(gltfMaterial.pbrMetallicRoughness == null || gltfMaterial.pbrMetallicRoughness.baseColorFactor == null)
+                    return;
+
+                if(gltfMaterial.pbrMetallicRoughness.baseColorFactor[0] == Defaults.EmissiveFactor[0]
+                    && gltfMaterial.pbrMetallicRoughness.baseColorFactor[1] == Defaults.EmissiveFactor[1]
+                    && gltfMaterial.pbrMetallicRoughness.baseColorFactor[2] == Defaults.EmissiveFactor[2])
+                {
+                    gltfMaterial.pbrMetallicRoughness.baseColorFactor = null;
+                    return;
+                }
+            }
+
+            if (gltfMaterial.pbrMetallicRoughness == null)
+                gltfMaterial.pbrMetallicRoughness = new GLTFPBRMetallicRoughness();
+
+            if (gltfMaterial.pbrMetallicRoughness.baseColorFactor == null)
+                gltfMaterial.pbrMetallicRoughness.baseColorFactor = new float[] {
+                    Defaults.EmissiveFactor[0], Defaults.EmissiveFactor[1], Defaults.EmissiveFactor[2], alpha };
+            else
+                gltfMaterial.pbrMetallicRoughness.baseColorFactor[3] = alpha;
+        }
+
+        public static void SetBaseColorFactor(this GLTFMaterial gltfMaterial, float r, float g, float b)
+        {
+            if (r == Defaults.EmissiveFactor[0] && g == Defaults.EmissiveFactor[1] && b == Defaults.EmissiveFactor[2])
+            {
+                if (gltfMaterial.pbrMetallicRoughness == null || gltfMaterial.pbrMetallicRoughness.baseColorFactor == null)
+                    return;
+
+                if (gltfMaterial.pbrMetallicRoughness.baseColorFactor[3] == 1.0f)
+                {
+                    gltfMaterial.pbrMetallicRoughness.baseColorFactor = null;
+                    return;
+                }
+            }
+
+            if (gltfMaterial.pbrMetallicRoughness == null)
+                gltfMaterial.pbrMetallicRoughness = new GLTFPBRMetallicRoughness();
+
+            if (gltfMaterial.pbrMetallicRoughness.baseColorFactor == null)
+                gltfMaterial.pbrMetallicRoughness.baseColorFactor = new float[] { r, g, b, Defaults.EmissiveFactor[3] };
+            else
+            {
+                gltfMaterial.pbrMetallicRoughness.baseColorFactor[0] = r;
+                gltfMaterial.pbrMetallicRoughness.baseColorFactor[1] = g;
+                gltfMaterial.pbrMetallicRoughness.baseColorFactor[2] = b;
+            }
+        }
+
+        public static void SetEmissiveFactor(this GLTFMaterial gltfMaterial, float r, float g, float b)
+        {
+            if (r == Defaults.EmissiveFactor[0] && g == Defaults.EmissiveFactor[1] && b == Defaults.EmissiveFactor[2])
+            {
+                gltfMaterial.emissiveFactor = null;
+                return;
+            }
+
+            if (gltfMaterial.emissiveFactor == null)
+            {
+                gltfMaterial.emissiveFactor = new float[] { r, g, b };
+            }
+            else
+            {
+                gltfMaterial.emissiveFactor[0] = r;
+                gltfMaterial.emissiveFactor[1] = g;
+                gltfMaterial.emissiveFactor[2] = b;
+            }
+        }
+
+        public static void SetRoughnessFactor(this GLTFMaterial gltfMaterial, float roughness)
+        {
+            if (roughness == Defaults.RoughnessFactor)
+            {
+                if (gltfMaterial.pbrMetallicRoughness == null)
+                    return;
+
+                gltfMaterial.pbrMetallicRoughness.roughnessFactor = null;
+                return;
+            }
+
+            if (gltfMaterial.pbrMetallicRoughness == null)
+                gltfMaterial.pbrMetallicRoughness = new GLTFPBRMetallicRoughness();
+            
+            gltfMaterial.pbrMetallicRoughness.roughnessFactor = roughness;
+        }
+
+        public static void SetMetallicFactor(this GLTFMaterial gltfMaterial, float metallic)
+        {
+            if (metallic == Defaults.MetallicFactor)
+            {
+                if (gltfMaterial.pbrMetallicRoughness == null)
+                    return;
+
+                gltfMaterial.pbrMetallicRoughness.metallicFactor = null;
+                return;
+            }
+
+            if (gltfMaterial.pbrMetallicRoughness == null)
+                gltfMaterial.pbrMetallicRoughness = new GLTFPBRMetallicRoughness();
+
+            gltfMaterial.pbrMetallicRoughness.metallicFactor = metallic;
+        }
+
+        #endregion
     }
 
 
