2e6873cd60c432c61136894bf83a32bd279a6f96
Luca Pierabella <lpierabella@asobostudio.com>
2019-05-14 16:14:00 +0200

    name selction from xls

---
+++ 3ds Max/Max2Babylon/Forms/SelectNameForm.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace Max2Babylon.Forms
+{
+    partial class SelectNameForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.preDefiniedNameList = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // preDefiniedNameList
+            // 
+            this.preDefiniedNameList.FormattingEnabled = true;
+            this.preDefiniedNameList.Location = new System.Drawing.Point(5, 5);
+            this.preDefiniedNameList.Margin = new System.Windows.Forms.Padding(0);
+            this.preDefiniedNameList.Name = "preDefiniedNameList";
+            this.preDefiniedNameList.Size = new System.Drawing.Size(264, 446);
+            this.preDefiniedNameList.TabIndex = 0;
+            this.preDefiniedNameList.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.preDefName_DoubleClick);
+            // 
+            // SelectNameForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(271, 458);
+            this.Controls.Add(this.preDefiniedNameList);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "SelectNameForm";
+            this.Text = "Babylon.js - Select Name";
+            this.Load += new System.EventHandler(this.SelectNameForm_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox preDefiniedNameList;
+    }
+}
---
+++ 3ds Max/Max2Babylon/Forms/SelectNameForm.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace Max2Babylon.Forms
+{
+    public partial class SelectNameForm : Form
+    {
+
+        private TextBox nameTextBox;
+
+        public SelectNameForm(TextBox _nameBox)
+        {
+            InitializeComponent();
+            nameTextBox = _nameBox;
+        }
+
+        private void SelectNameForm_Load(object sender, EventArgs e)
+        {
+            preDefiniedNameList.DataSource = GetExcelFile();
+        }
+
+        private List<string> GetExcelFile()
+        {
+            //Create COM Objects. Create a COM object for everything that is referenced
+            Excel.Application xlApp = new Excel.Application();
+            Excel.Workbook xlWorkbook = xlApp.Workbooks.Open(@"U:\Licences_Projets\FLIGHT_SIM\ART\Planes\NamingConventions.xlsx");
+            Excel._Worksheet xlWorksheet = xlWorkbook.Sheets[1];
+            Excel.Range xlRange = xlWorksheet.UsedRange;
+
+            int rowCount = xlRange.Rows.Count;
+            int colCount = xlRange.Columns.Count;
+            List<string> result = new List<string>();
+
+            int targetColumn = 7;
+            //excel is not zero based!!
+            for (int i = 2; i <= rowCount; i++)
+            {
+                if (xlRange.Cells[i, targetColumn] != null && xlRange.Cells[i, targetColumn].Value2 != null)
+                {
+                    string value = xlRange.Cells[i, targetColumn].Value2.ToString();
+                    result.Add(value);
+                }
+            }
+
+            //cleanup
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+
+            //rule of thumb for releasing com objects:
+            //  never use two dots, all COM objects must be referenced and released individually
+            //  ex: [somthing].[something].[something] is bad
+
+            //release com objects to fully kill excel process from running in the background
+            Marshal.ReleaseComObject(xlRange);
+            Marshal.ReleaseComObject(xlWorksheet);
+
+            //close and release
+            xlWorkbook.Close();
+            Marshal.ReleaseComObject(xlWorkbook);
+
+            //quit and release
+            xlApp.Quit();
+            Marshal.ReleaseComObject(xlApp);
+
+            return result;
+        }
+
+        private void preDefName_DoubleClick(object sender, MouseEventArgs e)
+        {
+            if (preDefiniedNameList.SelectedItem != null)
+            {
+                nameTextBox.Text = preDefiniedNameList.SelectedItem.ToString();
+            }
+        }
+    }
+}
