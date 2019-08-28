041a9bf8d04db4e589a08a4c0de61637a99fa0e7
elpie89 <pierabellaluca@gmail.com>
2019-08-28 16:57:13 +0200

    camer animation Fov

---
+++ 3ds Max/Max2Babylon/KittyHawk/KittyHawkCameraExtensions.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+using GLTFExport.Entities;
+
+namespace Max2Babylon.KittyHawkExtension
+{
+    [DataContract]
+    class GLTFExtensionCameraYFOV : GLTFProperty
+    {
+        public const string SerializedName = "ASOBO_animation_camera_yfov";
+        [DataMember(EmitDefaultValue = false)] public List<GLTFCameraChannel> channels;
+    }
+
+    [DataContract]
+    public class GLTFCameraChannel : GLTFProperty
+    {
+        [DataMember(IsRequired = true)]
+        public int sampler { get; set; }
+
+        [DataMember(IsRequired = true)]
+        public GLTFChannelCameraTarget target { get; set; }
+    }
+
+    [DataContract]
+    public class GLTFChannelCameraTarget : GLTFProperty
+    {
+        [DataMember(EmitDefaultValue = false)]
+        public int? camera { get; set; }
+
+        [DataMember(IsRequired = true)]
+        public string path { get; set; }
+    }
+}
--- 3ds Max/Max2Babylon/OnPostBuild.bat
+++ 3ds Max/Max2Babylon/OnPostBuild.bat
@@ -75,7 +75,7 @@ ECHO "No Configuaration"
 GOTO Close
 
 :DebugOnMax
-START /d "%max_location%" 3dsmax.exe "D:\KittyHawk\ASSETS\KittyHawk_Data\testExport.max"
+START /d "%max_location%" 3dsmax.exe "C:\Users\lpierabella\Desktop\cameraExport.max"
 GOTO Close
 
 :Close
